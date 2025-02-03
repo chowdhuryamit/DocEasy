0460605ed230ca6214f12be9a4d19ce41a26ef87
chowdhuryamit <camit8546@gmail.com>
2025-02-03 17:05:02 +0530

    write middlewares

---
+++ backend/src/middlewares/doctor.middleware.js
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken'
+
+const verifyJwtDoctor=async(req,res,next)=>{
+   const token=req.cookies._r
+   if(!token){
+    return res.status(401).json({msg:"Unauthorized request for doctor"})
+   }
+
+   const payload=jwt.verify(token,process.env.ACCESS_TOKEN_SECRET);
+
+   if(!payload){
+    return res.status(401).json({msg:"Invalid token for doctor"})
+   }
+
+   if(payload._id==process.env.DOCTOR_AUTH_SEC){
+    next();
+   }
+   else{
+    return res.status(401).json({msg:"Invalid token"})
+   }
+}
+
+export{
+    verifyJwtDoctor
+}
