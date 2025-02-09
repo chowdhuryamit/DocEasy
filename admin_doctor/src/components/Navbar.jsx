42a228d4f248dcb66a89e56a9d88d834c6442bd5
chowdhuryamit <camit8546@gmail.com>
2025-02-09 20:03:00 +0530

    navbar and sidebar

--- admin_doctor/src/components/Navbar.jsx
+++ admin_doctor/src/components/Navbar.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { assets } from '../assets/assets.js'
+import { doctorLogout } from '../store/doctorSlice.js'
+import { adminLogout } from '../store/adminSlice.js'
 
 const Navbar = () => {
+    const adminStatus = useSelector((state)=>state.Admin.status)
+    const doctorStatus = useSelector((state)=>state.Doctor.status)
+    const dispatch=useDispatch()
+
+    if(adminStatus){
+        dispatch(doctorLogout())
+    }
+    else{
+        dispatch(adminLogout())
+    }
+
+    const logout = async ()=>{
+
+    }
+
   return (
-    <div>
-      navbar
+    <div className='flex justify-between items-center px-4 sm:px-10 py-3 border-b bg-white'>
+      <div className='flex items-center gap-2 text-xs'>
+         <img src={assets.admin_logo} alt="admin_logo" className='w-36 sm:w-40 cursor-pointer' />
+         <p className='border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600'>{adminStatus?'Admin':'Doctor'}</p>
+      </div>
+      <button onClick={logout} className='bg-primary text-white text-sm px-5 py-2 rounded-full'>Logout</button>
     </div>
   )
 }
--- admin_doctor/src/components/Sidebar.jsx
+++ admin_doctor/src/components/Sidebar.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { NavLink } from 'react-router-dom'
+import { assets } from '../assets/assets.js'
 
 const Sidebar = () => {
+    const adminStatus = useSelector((state)=>state.Admin.status)
+    const doctorStatus = useSelector((state)=>state.Doctor.status)
   return (
-    <div>
-      sidebar
+    <div className='min-h-screen bg-white border-r'>
+      {
+        adminStatus && <ul className='text-[#515151] mt-5'>
+           <NavLink to={'/admin-dashboard'} className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary text-orange-500 font-bold' : ''}`}>
+             <img className='min-w-5' src={assets.home_icon} alt="home_icon" />
+             <p className='hidden md:block'>Dashboard</p>
+           </NavLink>
+           <NavLink to={'/all-appoinments'} className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary text-orange-500 font-bold' : ''}`}>
+             <img className='min-w-5' src={assets.appointment_icon} alt="appoinments_icon" />
+             <p className='hidden md:block'>Appoinments</p>
+           </NavLink>
+           <NavLink to={'/add-doctor'} className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary text-orange-500 font-bold' : ''}`}>
+             <img className='min-w-5' src={assets.add_icon} alt="add_icon" />
+             <p className='hidden md:block'>Add Doctor</p>
+           </NavLink>
+           <NavLink to={'/doctors-list'} className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary text-orange-500 font-bold' : ''}`}>
+             <img className='min-w-5' src={assets.people_icon} alt="people_icon" />
+             <p className='hidden md:block'>Doctors List</p>
+           </NavLink>
+        </ul>
+      }
+      {
+        doctorStatus && <ul className='text-[#515151] mt-5'>
+           <NavLink to={'/doctor-dashboard'} className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary text-orange-500 font-bold' : ''}`}>
+             <img className='min-w-5' src={assets.home_icon} alt="home_icon" />
+             <p className='hidden md:block'>Dashboard</p>
+           </NavLink>
+           <NavLink to={'/doctor-appoinments'} className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary text-orange-500 font-bold' : ''}`}>
+             <img className='min-w-5' src={assets.appointment_icon} alt="appoinments_icon" />
+             <p className='hidden md:block'>Appoinments</p>
+           </NavLink>
+           <NavLink to={'/doctor-profile'} className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary text-orange-500 font-bold' : ''}`}>
+             <img className='min-w-5' src={assets.people_icon} alt="people_icon" />
+             <p className='hidden md:block'>Profile</p>
+           </NavLink>
+        </ul>
+      }
     </div>
   )
 }
